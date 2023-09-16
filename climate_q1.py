bd88b0b27060970d0b3a900902bf39634f85c676
Mitch Howard <mitchell.howard2@griffithuni.edu.au>
2023-09-16 16:58:23 +1000

    climate_q1.py

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,38 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Create empty lists to store data
 years = []
 co2 = []
+
 temp = []
+
+try:
+    # Execute an SQL query to fetch data
+    cursor.execute("SELECT years, c02, temp FROM Climate Data")
+
+    # Fetch the data from the query result
+    data = cursor.fetchall()
+
+    # Populate the lists with corresponding values
+    for row in data:
+        years, c02, temp = row
+        years.append(years)
+        c02.append(c02)
+        temp.append(temp)
+
+    # Close the database connection
+    conn.close()
+
+    # Now you have the data in the lists
+    # You can work with the data as needed
+except sqlite3.Error as e:
+    print("SQLite error:", e)
+    conn.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
