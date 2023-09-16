a4c3c39eceb020767a45cd7984fe5843ed4048e7
Mitch Howard <mitchell.howard2@griffithuni.edu.au>
2023-09-16 17:50:03 +1000

    Making changes to the climate_q2.py file, commiting and pushing t the server

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#load data 
+downloadfile = pd.read_csv('climate.csv')
+
 years = []
 co2 = []
 temp = []
+
+#extract the C02 and YEAR x/y columns from the table
+gases = downloadfile('C02')
+years = downloadfile('Year (decade)')
+
+
+#create bar chart
+plt.figure(figsize=(15,8))
+plt.bar(gases, years, color='red')
+plt.title('Bar Chart for Gases & Years')
+plt.xlabel("Year (decade)")
+plt.ylabel("[C02]")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
